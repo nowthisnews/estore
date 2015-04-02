ce770169531825ea521b849154f1dce9f0976260
Héctor Ramón Jiménez <hector0193@gmail.com>
2015-04-02 20:09:29 +0200

    More readable integration test

--- spec/integration/session_spec.rb
+++ spec/integration/session_spec.rb
@@ -33,74 +33,76 @@ describe Estore::Session do
     "test-#{SecureRandom.uuid}"
   end
 
-  def populate(count, stream = nil)
+  def stream_with(count, stream = nil)
     stream ||= random_stream
     session.append(stream, events(count)).sync
     stream
   end
 
-  it 'reads events from a stream' do
-    stream = populate(20)
-    read = session.read(stream, 0, 20).sync
-
-    expect(read.events.size).to be(20)
-
-    read.events.each_with_index do |event, index|
-      expect(parse_data(event)).to eql('id' => index)
+  RSpec::Matchers.define :start_from do |start|
+    match do |events|
+      events.each_with_index do |event, index|
+        expect(parse_data(event)).to eql('id' => index + start)
+      end
     end
+  end
+
+  it 'reads an interval of events from stream' do
+    read = session.read(stream_with(30), 15, 15).sync
+
+    expect(read.events.size).to be(15)
+    expect(read.events).to start_from(15)
   end
 
   it 'allows to make a live subscription' do
     stream = random_stream
-    received = 0
+    received = []
+
+    stream_with(20, stream)
 
     sub = session.subscription(stream)
     sub.on_error { |error| raise error.inspect }
-
-    sub.on_event do |event|
-      expect(parse_data(event)).to eql('id' => received)
-      received += 1
-    end
-
+    sub.on_event { |event| received << event }
     sub.start
 
-    populate(50, stream)
+    stream_with(50, stream)
 
     Timeout.timeout(5) do
       loop do
-        break if received >= 50
+        break if received.size >= 50
         sleep(0.1)
       end
     end
+
+    expect(received.size).to be(50)
+    expect(received).to start_from(20)
   end
 
   it 'allows to make a catchup subscription' do
     stream = random_stream
-    received = 0
+    received = []
 
-    populate(50, stream)
+    stream_with(50, stream)
 
     sub = session.subscription(stream, catch_up_from: 20)
     sub.on_error { |error| raise error.inspect }
-
-    sub.on_event do |event|
-      expect(parse_data(event)).to eql('id' => received + 20)
-      received += 1
-    end
-
+    sub.on_event { |event| received << event }
     sub.start
 
     Thread.new do
       50.times do
-        populate(2, stream)
+        stream_with(2, stream)
       end
     end
 
     Timeout.timeout(5) do
       loop do
-        break if received >= 130
+        break if received.size >= 130
         sleep(0.1)
       end
     end
+
+    expect(received.size).to be(130)
+    expect(received).to start_from(20)
   end
 end
